76993fbd165eb5361445743d2209c37e462a2a86
guto <gustavomgamer3@hotmail.com>
2022-06-20 14:40:00 -0300

    added first entity class

---
+++ src/main/java/com/guto/project/domain/entity/Person.java
@@ -0,0 +1,39 @@
+package com.guto.project.domain.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "person")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "age", nullable = false)
+    private Integer age;
+
+    public Person() {
+    }
+
+    public Person(Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+}
