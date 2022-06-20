b97c7de60651088e21c4bce5fde202ee83730d86
guto <gustavomgamer3@hotmail.com>
2022-06-20 18:32:48 -0300

    small fix to Person entity

--- src/main/java/com/guto/project/domain/entity/Person.java
+++ src/main/java/com/guto/project/domain/entity/Person.java
@@ -10,7 +10,7 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "name", length = 50, nullable = false)
+    @Column(name = "name", length = 50, nullable = false, unique = true)
     private String name;
 
     @Column(name = "age", nullable = false)
